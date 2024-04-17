88e052be47bcb3aada5ce13de8994de3f4980fbd
Siddique Ahmad <siddiqueahmad956@gmail.com>
2024-04-17 23:17:58 +0500

    done

--- App.js
+++ App.js
@@ -1,7 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View,Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging'
 
 export default function App() {
+
+  const requestUserPermission=async()=>{
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+//   useEffect(()=>{
+// if(requestUserPermission())
+// {
+//   messaging().getToken().then(token=>{
+//     console.log(token)
+//   })
+// }
+// else
+// {
+//   console.log("Failed Token",authStatus)
+// }
+// messaging()
+// .getInitialNotification()
+// .then(async (remoteMessage)=>{
+//   if(remoteMessage)
+//   {
+//     console.log(
+//       'Notification caused app to open from quit state:',
+//       remoteMessage.notification,
+//     )
+//   }
+// })
+// messaging().onNotificationOpenedApp(async (remoteMessage)=>{
+//   console.log(
+//     'Notification caused app to open from background state:',
+// remoteMessage.notification
+//   )
+// })
+// messaging().setBackgroundMessageHandler(async remoteMessage=>{
+//   console.log('Message handled in the background',remoteMessage)
+// })
+// const unsubscribe = messaging().onMessage(async remoteMessage => {
+//   Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+// });
+
+// return unsubscribe;
+
+//   },[])
+
+const getToken=async()=>{
+  const token=await messaging().getToken()
+  console.log("Token=",token)
+}
+useEffect(()=>{
+messaging().setBackgroundMessageHandler(async remoteMessage=>{
+  console.log('Message handled in the background',remoteMessage)
+})
+
+  requestUserPermission()
+getToken()
+},[])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
